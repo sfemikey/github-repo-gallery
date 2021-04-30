2998730d78ee519224d57155c02f2e8b94eff631
sfemikey <femikeyme01@yahoo.com>
2021-04-30 09:36:46 -0600

    Added and fetch my repos list

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 //Global variable where your profile information will appear
 const overview = document.querySelector(".overview");
-    const username = "sfemikey";
+const username = "sfemikey";
+//create variable to select unordered list to display the repos list
+const repoList = document.querySelector(".repo-list");
+
 
 //create async function to fetch info from your github api 
-const gitUserInfo = async function () {
+const fetchUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
     //call the function displaying the user info
     displayUserInfo(data);
 };
 //call the function to see your results
-gitUserInfo();
+fetchUserInfo();
 
 //display the fetched user information on the page
 const displayUserInfo = function (data) {
@@ -27,11 +30,33 @@ const displayUserInfo = function (data) {
       <p><strong>Bio:</strong> ${data.bio}</p>
       <p><strong>Location:</strong> ${data.location}</p>
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-    </div> 
-    `;
+    </div>
+  `;
     //append the div to the overview element
     overview.append(div);
+    gitFetchRepos();
 };
+
+//function to fetch your repos
+const gitFetchRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos)  {
+    //loop and create a list item for each repo
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        // give each item a class of "repo" and <h3> element with the repo name
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        //append the list item to the global variable that sele
+        repoList.append(repoItem);
+    }
+};
+
+
 
 
 
